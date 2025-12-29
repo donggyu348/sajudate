29afe32733e9a39bcde130a652fd74be24cb8422
ldg <ldg9742@naver.com>
2025-12-29 18:09:59 +0900

    카카오페이 추가

--- src/framework/web/enums/Platform.js
+++ src/framework/web/enums/Platform.js
@@ -1,5 +1,5 @@
 
 export const Platform = Object.freeze({
   JUJANGSO: { code: "JUJANGSO", domain: "https://saju-maeul.kr", fileDir: "jujangso" },
-  TIGHT: { code: "TIGHT", domain: "http://sajudate.store", fileDir: "tight" },
+  TIGHT: { code: "TIGHT", domain: "https://sajudate.store", fileDir: "tight" },
 });
