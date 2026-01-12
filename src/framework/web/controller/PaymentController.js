2d54c7de2afeed3c51d654a347909a0c7b7689b6
ldg <ldg9742@naver.com>
2026-01-12 18:23:23 +0900

    금액 변경

--- src/framework/web/controller/PaymentController.js
+++ src/framework/web/controller/PaymentController.js
@@ -90,7 +90,7 @@ const PaymentController = {
       const TEST_PHONE_NUMBER = [
         "01055989742",   "01058389701" , "01033336666"
       ];
-      const TEST_AMOUNT = 1;
+      const TEST_AMOUNT = 100;
 
       const { payMethod, reportHistoryId, userTelNo } = req.body;
 
