8590578b9291d5351c665307fffcf5fc7753ff3c
ldg <ldg9742@naver.com>
2026-01-12 18:20:46 +0900

    라이브 키 추가

--- src/framework/web/api/TossPaymentsClient.js
+++ src/framework/web/api/TossPaymentsClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // 시크릿 키 (상점 관리자에서 발급받은 실제 키 사용)
-const SECRET_KEY = "test_sk_AQ92ymxN3495e5JRDnG4VajRKXvd"; 
+const SECRET_KEY = "live_sk_4yKeq5bgrpekleJXRl1J8GX0lzW6"; 
 const ENCODED_KEY = Buffer.from(SECRET_KEY + ":").toString("base64");
 
 class TossPaymentsClient {
