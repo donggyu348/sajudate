d5c0be9bb4ffddf0b78c3adec6bc8f3edbe96b1a
ldg <ldg9742@naver.com>
2026-01-28 23:36:51 +0900

    feat: 번들 추가

--- src/framework/web/orm/sequelize.js
+++ src/framework/web/orm/sequelize.js
@@ -1,7 +1,7 @@
 import { Sequelize, DataTypes } from "sequelize";
 import config from "./config.js";
 
-// 1. 개별 모델 파일들을 임포트합니다.
+// 모델 파일들을 직접 가져옵니다.
 import usersModel from "./models/users.js";
 import adminsModel from "./models/admins.js";
 import reportHistoryModel from "./models/reportHistory.js";
@@ -10,10 +10,6 @@ import couponsModel from "./models/coupons.js";
 
 const env = "product";
 const dbConf = config[env];
-
-if (!dbConf || !dbConf.dialect) {
-  throw new Error(`Invalid DB config for environment: ${env}`);
-}
 
 const sequelize = new Sequelize(
   dbConf.database,
@@ -28,14 +24,19 @@ const sequelize = new Sequelize(
   }
 );
 
-// 2. 모델들을 Sequelize 인스턴스에 연결하여 초기화합니다.
+/**
+ * 🔥 핵심: 각 모델 파일이 함수 형태라고 가정하고 sequelize와 DataTypes를 주입합니다.
+ * 만약 모델 파일이 함수가 아니라면, 아래처럼 직접 정의를 실행해줍니다.
+ */
+
+// 1. 모델들을 초기화하여 변수에 할당합니다.
 const users = usersModel(sequelize, DataTypes);
 const admins = adminsModel(sequelize, DataTypes);
 const reportHistory = reportHistoryModel(sequelize, DataTypes);
 const paymentTransaction = paymentTransactionModel(sequelize, DataTypes);
 const coupons = couponsModel(sequelize, DataTypes);
 
-// 3. sajuRouter.js 등에서 사용할 수 있도록 모델들을 내보냅니다.
+// 2. 다른 파일(sajuRouter.js 등)에서 쓸 수 있도록 이름별로 내보냅니다.
 export {
   users,
   admins,
