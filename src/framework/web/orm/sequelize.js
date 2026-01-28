6b987d48dfc94b4a8dae2d27fd54190c9aaca420
ldg <ldg9742@naver.com>
2026-01-28 23:35:21 +0900

    feat: 번들 추가

--- src/framework/web/orm/sequelize.js
+++ src/framework/web/orm/sequelize.js
@@ -1,8 +1,13 @@
-import { Sequelize } from "sequelize";
-
+import { Sequelize, DataTypes } from "sequelize";
 import config from "./config.js";
 
-// const env = process.env.NODE_ENV || "local";
+// 1. 개별 모델 파일들을 임포트합니다.
+import usersModel from "./models/users.js";
+import adminsModel from "./models/admins.js";
+import reportHistoryModel from "./models/reportHistory.js";
+import paymentTransactionModel from "./models/paymentTransaction.js";
+import couponsModel from "./models/coupons.js";
+
 const env = "product";
 const dbConf = config[env];
 
@@ -22,5 +27,21 @@ const sequelize = new Sequelize(
     logging: dbConf.logging
   }
 );
+
+// 2. 모델들을 Sequelize 인스턴스에 연결하여 초기화합니다.
+const users = usersModel(sequelize, DataTypes);
+const admins = adminsModel(sequelize, DataTypes);
+const reportHistory = reportHistoryModel(sequelize, DataTypes);
+const paymentTransaction = paymentTransactionModel(sequelize, DataTypes);
+const coupons = couponsModel(sequelize, DataTypes);
+
+// 3. sajuRouter.js 등에서 사용할 수 있도록 모델들을 내보냅니다.
+export {
+  users,
+  admins,
+  reportHistory,
+  paymentTransaction,
+  coupons
+};
 
 export default sequelize;
