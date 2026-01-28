bfa8596d227e824d64a27bae46e2853d7c18eb70
ldg <ldg9742@naver.com>
2026-01-28 23:38:03 +0900

    feat: 번들 추가

--- src/framework/web/orm/sequelize.js
+++ src/framework/web/orm/sequelize.js
@@ -1,12 +1,7 @@
 import { Sequelize, DataTypes } from "sequelize";
 import config from "./config.js";
-
-// 모델 파일들을 직접 가져옵니다.
-import usersModel from "./models/users.js";
-import adminsModel from "./models/admins.js";
-import reportHistoryModel from "./models/reportHistory.js";
-import paymentTransactionModel from "./models/paymentTransaction.js";
-import couponsModel from "./models/coupons.js";
+import usersModel from "./models/users.js"; // 추가
+import couponsModel from "./models/coupons.js"; // 추가
 
 const env = "product";
 const dbConf = config[env];
@@ -24,25 +19,8 @@ const sequelize = new Sequelize(
   }
 );
 
-/**
- * 🔥 핵심: 각 모델 파일이 함수 형태라고 가정하고 sequelize와 DataTypes를 주입합니다.
- * 만약 모델 파일이 함수가 아니라면, 아래처럼 직접 정의를 실행해줍니다.
- */
-
-// 1. 모델들을 초기화하여 변수에 할당합니다.
-const users = usersModel(sequelize, DataTypes);
-const admins = adminsModel(sequelize, DataTypes);
-const reportHistory = reportHistoryModel(sequelize, DataTypes);
-const paymentTransaction = paymentTransactionModel(sequelize, DataTypes);
-const coupons = couponsModel(sequelize, DataTypes);
-
-// 2. 다른 파일(sajuRouter.js 등)에서 쓸 수 있도록 이름별로 내보냅니다.
-export {
-  users,
-  admins,
-  reportHistory,
-  paymentTransaction,
-  coupons
-};
+// 문제가 되는 모델들을 여기서 초기화해서 내보냅니다.
+export const users = usersModel(sequelize, DataTypes);
+export const coupons = couponsModel(sequelize, DataTypes);
 
 export default sequelize;
