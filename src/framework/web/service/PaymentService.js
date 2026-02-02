0af02503eda25e07afd932fbdcff4938dfb3511d
ldg <ldg9742@naver.com>
2026-02-02 16:04:17 +0900

    feat: 번들 로직 추가

--- src/framework/web/service/PaymentService.js
+++ src/framework/web/service/PaymentService.js
@@ -68,7 +68,7 @@
 
   async approveTossPayment(payload) {
   const { paymentKey, orderId, amount, userTelNo, userPw,goodsType } = payload;
-
+console.log(`[LOG 4][Service_Approve] 시작 - orderId: ${orderId}, goodsType: ${goodsType}`);
   // 1. 토스 서버에 최종 승인 요청 (TossPaymentsClient 활용)
   // TossPaymentsClient.confirmPayment가 내부적으로 axios post를 호출해야 합니다.
   const result = await TossPaymentsClient.confirmPayment({
