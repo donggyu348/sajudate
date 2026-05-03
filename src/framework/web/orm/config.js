4e21f70eeb3b2b81f0233230c61f6b388d22907c
ldg <ldg9742@naver.com>
2026-05-03 22:22:20 +0900

    디비 분리

--- src/framework/web/orm/config.js
+++ src/framework/web/orm/config.js
@@ -27,7 +27,7 @@ export default {
     product: {
         username: 'dbmasteruser',
         password: ':9p}jU,,Qs6PGaeur)nuVX#h}+Y{7!Al',
-        database: 'new_site_db',
+        database: 'dbmaster1',
         host: 'ls-bf6a633560df15b5ac396f6c91f990459393b811.ch2egicsosuj.ap-northeast-2.rds.amazonaws.com',
         port: 3306,
         dialect: "mysql",
